f853929dfa53ba473d093f63af16af4e01502416
Xander van Rijn <xandervanrijn@outlook.com>
2013-08-25 15:53:09 +0200

    Initial SpecFlow import
    Related Work Items: #13

---
+++ Tessler.SpecFlow/BaseFeature.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoSupport.Tessler.SpecFlow
+{
+    [TestClass]
+    public abstract class BaseFeature
+    {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+    }
+}
