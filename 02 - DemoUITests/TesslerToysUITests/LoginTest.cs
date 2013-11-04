9780f7403025311f652f94fae573430bf81b1569
marcoo <marcoo@infosupport.com>
2013-11-04 10:21:03 +0100

    Added demo project

---
+++ 02 - DemoUITests/TesslerToysUITests/LoginTest.cs
@@ -0,0 +1,32 @@
+﻿using InfoSupport.Tessler.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TesslerToysUITests
+{
+    [TestClass]
+    public class LoginTest : TestBase
+    {
+        /// <summary>
+        /// Dit is een voorbeeld test, waar we gebruik kunnen maken van de fluent-api.
+        /// </summary>
+        [TestMethod]
+        public void LoginSuccessTest()
+        {
+            TesslerToys
+                .MetH3Tekst(a => a.AssertEqual("Welcome to Tessler Toys!"))
+                .KiesUpdateText()
+                .MetH3Tekst(a => a.AssertEqual("Automating Fun Since 1989!"))
+
+                .MetProduct(2)
+                    .MetName(a => a.AssertEqual("Lego technic truck"))
+                    .MetPrice(a => a.AssertEqual("55,00"))
+                    .MetInStock(a => a.AssertEqual("23"))
+                    .Then()
+
+                .KiesPage2()
+
+                .MetTitel(a => a.AssertEqual("Page 2"))
+            ;
+        }
+    }
+}
