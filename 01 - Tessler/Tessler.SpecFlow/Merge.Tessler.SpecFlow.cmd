3015d892e7880d2242adf36ad0ad1383bccb3756
Marco vd Oever <marco@flyingpie.nl>
2014-09-09 13:12:30 +0000

    Updated script names

--- 01 - Tessler/Tessler.SpecFlow/Merge.Tessler.SpecFlow.cmd
+++ 01 - Tessler/Tessler.SpecFlow/Merge.Tessler.SpecFlow.cmd
@@ -1,9 +1,9 @@
 ECHO Create a single dll to add in the NuGet package
 
-IF EXIST "SpecFlow.Merged" rmdir /s /q "SpecFlow.Merged"
-mkdir "SpecFlow.Merged"
+IF EXIST "Merged.Tessler.SpecFlow" rmdir /s /q "Merged.Tessler.SpecFlow"
+mkdir "Merged.Tessler.SpecFlow"
 
-ILRepack.exe /target:library /out:"SpecFlow.Merged\TesslerGeneratorProvider.Generator.SpecFlowPlugin.dll" ^
+ILRepack.exe /target:library /out:"Merged.Tessler.SpecFlow\TesslerGeneratorProvider.Generator.SpecFlowPlugin.dll" ^
  TesslerGeneratorProvider.Generator.SpecFlowPlugin.dll ^
  TechTalk.SpecFlow.Generator.dll ^
  TechTalk.SpecFlow.Parser.dll ^
--- 01 - Tessler/Tessler/Merge.Tessler.cmd
+++ 01 - Tessler/Tessler/Merge.Tessler.cmd
@@ -1,9 +1,9 @@
 ECHO Create a single dll to add in the NuGet package
 
-IF EXIST "Merged" rmdir /s /q "Merged"
-mkdir "Merged"
+IF EXIST "Merged.Tessler" rmdir /s /q "Merged.Tessler"
+mkdir "Merged.Tessler"
 
-ILRepack.exe /target:library /out:"Merged\InfoSupport.Tessler.dll" ^
+ILRepack.exe /target:library /out:"Merged.Tessler\InfoSupport.Tessler.dll" ^
  InfoSupport.Tessler.dll ^
  log4net.dll ^
  Microsoft.Practices.ServiceLocation.dll ^
